dbc7d36380823a56643fd8536bcfa7ccb4944fd1
Charger1256A <aditya.ram112005@gmail.com>
2024-06-11 17:18:50 -0700

    connected to mongodb

--- server.py
+++ server.py
@@ -5,10 +5,39 @@ Backend for study app.
 '''
 
 from fastapi import FastAPI
+import os
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+
+load_dotenv()
+
+# atlas url for connection
+CONNECTION_STRING = os.getenv('MONGODB_CONNECTION')
+
+# Create a connection using MongoClient.
+client = MongoClient(CONNECTION_STRING)
+db = client["study-app"]
 
 app = FastAPI()
 
-# Template default route
+'''
+Template default route
+'''
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Study App Default Route"}
+
+'''
+Allows users to signup
+'''
+@app.post("/signup")
+async def signup():
+    user_collection = db["users"]
+    test_user = {
+        "username": "test_user",
+        "password": "abcd",
+        "decks": [],
+    }
+    user_collection.insert_one(test_user)
+    pass
