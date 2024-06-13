687f69a6fe094801a498d9a9916ac97534904ef3
Charger1256A <aditya.ram112005@gmail.com>
2024-06-12 20:59:50 -0700

    Finished login route

--- server.py
+++ server.py
@@ -4,24 +4,29 @@ Author: Aditya Ramanathan
 Description: Backend for study app.
 '''
 
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, Response, Request
 import os
 from dotenv import load_dotenv
 from pymongo import MongoClient
+from starlette.middleware.sessions import SessionMiddleware
 
 from body_schemas.user import User
-from utilities import hash_password
+from utilities import hash_password, passwords_match
 
 load_dotenv()
 
 # atlas url for connection
 CONNECTION_STRING = os.getenv('MONGODB_CONNECTION')
 
-# Create a connection using MongoClient.
+# create a connection using MongoClient.
 client = MongoClient(CONNECTION_STRING)
 db = client["study-app"]
 
 app = FastAPI()
+
+# creates session
+SESSION_KEY = os.getenv("SESSION_KEY")
+app.add_middleware(SessionMiddleware, secret_key=SESSION_KEY)
 
 @app.get("/")
 async def root():
@@ -41,17 +46,19 @@ async def signup(user: User, response: Response):
     '''
     # gets data from body
     username = user.username
-    password = hash_password(user.password)
+    password = user.password
+
+    password, salt = hash_password(password)
 
     # gets user collection
     user_collection = db["users"]
 
-    # return bad status code if username or password does not exist.
+    # return bad status code if username or password does not exist
     if username == "" or password == "":
         response.status_code = 400
         return {"message": "username and password must exist."}
 
-    # TODO: return bad status code if user already exists
+    # return bad status code if user already exists
     existing_user = user_collection.find_one({"username": username})
     if existing_user != None:
         response.status_code = 400
@@ -61,6 +68,7 @@ async def signup(user: User, response: Response):
     test_user = {
         "username": username,
         "password": password,
+        "salt": salt,
         "decks": [],
     }
     # inserts object into collection
@@ -68,4 +76,42 @@ async def signup(user: User, response: Response):
 
     # TODO: return good status code if successful
     return {"message": "user successfully created."}
+
+@app.post("/login", status_code=200)
+async def login(user: User, response: Response, request: Request):
+    '''
+    Allows users to login
+
+    body:
+        user: the user to create an account for -> User
+    '''
+    # gets data from body
+    username = user.username
+    password = user.password
+
+    # gets user collection
+    user_collection = db["users"]
+
+    # return bad status code if username or password does not exist
+    if username == "" or password == "":
+        response.status_code = 400
+        return {"message": "username and password must exist."}
+
+    # return bad status code if username or password do not match
+    existing_user = user_collection.find_one({"username": username})
+    if existing_user == None or not passwords_match(password, existing_user["password"], existing_user["salt"]):
+        response.status_code = 400
+        return {"message": "Incorrect credentials."}
+
+    # check if user is already logged in
+    if "user" in request.session:
+        return {"message": "user already signed in."}
+
+    
+    # store user in session 
+    request.session["user"] = existing_user["username"]
+    return {"message": "user signed in."}
+
+    
+
     
--- utilities.py
+++ utilities.py
@@ -6,16 +6,34 @@ Description: contains utility function required for backend.
 import os
 import hashlib
 
-def hash_password(password: str):
+def hash_password(password: str, salt=None):
     '''
-    Given a password this function will return a hashed version of it.
+    Given a password this function will return a hashed version of it. If no salt is provided a new salt will be generated.
 
     Args:
         password: the password to be hashed -> str
+        salt: the salt used in the hash function -> bytes
 
-    Returns: hashed version of password -> bytes 
+    Returns: 
+        - hashed version of password -> bytes 
+        - salt -> bytes
 
     '''
-    salt = os.urandom(32)
+    if salt == None:
+        salt = os.urandom(32)
+    
     password = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
-    return password
+    return password, salt
+
+def passwords_match(password: str, hashed_password: bytes, salt: bytes):
+    '''
+    Checks if given password matches hashed password
+
+    Args:
+        password: the password to be hashed -> str
+        hashed_password: the hashed password to compare password with
+        salt: the salt used to hash password
+
+    Returns: true if passwords match, false otherwise -> bool
+    '''
+    return hash_password(password, salt)[0] == hashed_password
